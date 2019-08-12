ae5538b8a39fecdc3c168db7bf060d8309b66cb2
CountChu <visualge@gmail.com>
2019-08-13 00:46:39 +0800

    1) Add -t option 2) Provide TestImages and TestVideo 3) Update README.md

--- PlayImages/README.md
+++ PlayImages/README.md
@@ -3,25 +3,34 @@ The Python PlayImages.py plays images or video optionally step by step. The app 
 
 ## Usage
 ```
-Usage: PlayImages.py [-h] [-v] [--video VIDEOFN] [--images IMAGESDIR]
+usage: PlayImages.py [-h] [-v] [--video VIDEOFN] [--images IMAGESDIR]
                      [--begin BEGINNUM] [--end ENDNUM] [--step]
                      [--goto GOTONUM] [-r] [--log LOGFN]
                      [--ri RECORDIMAGESDIR] [--fps] [--bn] [--sd SLOWDOWN]
+                     [-t]
 
 The app plays images or video optionally step by step.
 
-Usage 1: python PlayImages.py --images Test1 --step
-    Play images in the directory Test1 step by step
+Usage 1: python PlayImages.py --video TestVideo/video.MP4 --step
+    Play the video file step by step.
 
-Usage 2: python PlayImages.py --images Test1 --fps -r
-    Play images in the directory Test1 with FPS and combined them in a video out.mp4.
+Usage 2: python PlayImages.py --video TestVideo/video.MP4 --ri TestImages
+    Play the video file and save frames in the TestImages directory.
 
-Usage 3: python PlayImages.py --fps -r
+Usage 3: python PlayImages.py --images TestImages --step
+    Play images in the directory TestImages step by step.
+
+Usage 4: python PlayImages.py --images TestImages --fps -r
+    Play images in the directory TestImages with FPS and combined them in a
+    video out.mp4.
+
+Usage 5: python PlayImages.py --images TestImages --step -t
+    Play images in the directory TestImages step by step and transform each
+    frame by calling Util.transform(). You can modify the function to develop
+    your specific application of computer vision.
+
+Usage 6: python PlayImages.py --fps -r
     Open camera with FPS and save frames in out.mp4.
-
-Usage 4: python PlayImages.py --video Test1.mp4 --ri Output
-    Play the video Test1.mp4. You can modify the app to handle the video,
-    and output the result images in the directory Output.
 
 optional arguments:
   -h, --help            show this help message and exit
@@ -38,4 +47,23 @@ optional arguments:
   --fps                 Display fps.
   --bn                  Display base name.
   --sd SLOWDOWN         It follows -r. It slows down the output video by specifying a number of repeated frames
+  -t                    Enable transformation.
 ```
+
+## Util.py
+### def transform(frame)
+
+The function provides examples of cv2 functions as below.
+- cv2.rectangle()
+- cv2.circle()
+- cv2.putText()
+- cv2.line()
+
+You can try it by run the command.
+```
+python PlayImages.py --images TestImages --step -t
+```
+
+The you can see the green or red texts and pictures on the below frame.
+
+![Image](Doc/Demo1.png)
--- PlayImages/Util.py
+++ PlayImages/Util.py
@@ -3,7 +3,7 @@
 #       Util.py - Utility Module.
 #
 # FUNCTIONAL DESCRIPTION.
-#       The module provide app with API. 
+#       The module provide app with API.
 #
 # NOTICE.
 #       Author: visualge@gmail.com (CountChu)
@@ -25,25 +25,25 @@ import cv2
 # It handles them.
 #
 
-def handle(frame):
+def transform(frame):
 
     #
     # Define colors
     #
-    
+
     green = (0, 255, 0)                 # green
-    red = (0, 0, 255)                   # red   
+    red = (0, 0, 255)                   # red
     white = (255, 255, 255)             # white
-    
+
     #
     # Define the layout.
     #
-    
-    x0 = 10
-    y0 = 20
+
+    x0 = 200
+    y0 = 300
     dx = 20
-    dy = 20    
-    
+    dy = 20
+
     #
     # Draw rectangles
     #
@@ -53,82 +53,82 @@ def handle(frame):
     w = 30
     h = 40
     cv2.rectangle(frame, (x, y, w, h), green)
-    
+
     y += h + 10
-    cv2.rectangle(frame, (x, y, w, h), green, cv2.FILLED)    
-    
+    cv2.rectangle(frame, (x, y, w, h), green, cv2.FILLED)
+
     #
     # Draw circles
     #
-    
+
     x += (w+20)
     y = y0
     cv2.circle(frame, (x, y), 2, red, -1)
-    
-    y += dy
-    cv2.circle(frame, (x, y), 4, red, -1)  
 
     y += dy
-    cv2.circle(frame, (x, y), 6, red, -1)     
-    
+    cv2.circle(frame, (x, y), 4, red, -1)
+
+    y += dy
+    cv2.circle(frame, (x, y), 6, red, -1)
+
     y += dy
     cv2.circle(frame, (x, y), 2, red, 1)
-    
-    y += dy
-    cv2.circle(frame, (x, y), 4, red, 1)  
 
     y += dy
-    cv2.circle(frame, (x, y), 6, red, 1)  
+    cv2.circle(frame, (x, y), 4, red, 1)
+
+    y += dy
+    cv2.circle(frame, (x, y), 6, red, 1)
 
     y += dy
     cv2.circle(frame, (x, y), 2, red, 2)
-    
-    y += dy
-    cv2.circle(frame, (x, y), 4, red, 2)  
 
     y += dy
-    cv2.circle(frame, (x, y), 6, red, 2)   
+    cv2.circle(frame, (x, y), 4, red, 2)
+
+    y += dy
+    cv2.circle(frame, (x, y), 6, red, 2)
 
     #
     # Draw texts
-    #    
-    
+    #
+
     x += dx
     y = y0
     cv2.putText(frame, "Test", (x, y), cv2.FONT_HERSHEY_COMPLEX, 0.4, green)
 
     y += dy
     cv2.putText(frame, "Test it.", (x, y), cv2.FONT_HERSHEY_SIMPLEX, 0.4, green)
-    
-    y += dy
-    cv2.putText(frame, "Test it.", (x, y), cv2.FONT_HERSHEY_SIMPLEX, 0.5, green)   
 
     y += dy
-    cv2.putText(frame, "Test it.", (x, y), cv2.FONT_HERSHEY_SIMPLEX, 0.6, green)  
+    cv2.putText(frame, "Test it.", (x, y), cv2.FONT_HERSHEY_SIMPLEX, 0.5, green)
+
+    y += dy
+    cv2.putText(frame, "Test it.", (x, y), cv2.FONT_HERSHEY_SIMPLEX, 0.6, green)
 
     y += dy
     cv2.putText(frame, "Test it.", (x, y), cv2.FONT_HERSHEY_SIMPLEX, 0.4, green, 2)
-    
+
     y += dy
-    cv2.putText(frame, "Test it.", (x, y), cv2.FONT_HERSHEY_SIMPLEX, 0.5, green, 2)   
+    cv2.putText(frame, "Test it.", (x, y), cv2.FONT_HERSHEY_SIMPLEX, 0.5, green, 2)
 
     y += dy
     cv2.putText(frame, "Test it.", (x, y), cv2.FONT_HERSHEY_SIMPLEX, 0.6, green, 2)
 
     #
     # Draw lines
-    #     
-    
+    #
+
     x += 80
-    
-    y = y0    
+
+    y = y0
     cv2.line(frame, (x, y), (x+40, y+10), red, 1)
-    
+
     y += dy
-    cv2.line(frame, (x, y), (x+40, y+10), red, 2)   
-    
-    y += dy    
+    cv2.line(frame, (x, y), (x+40, y+10), red, 2)
+
+    y += dy
     cv2.line(frame, (x, y), (x+40, y+10), red, 1, cv2.LINE_AA)
-    
+
     y += dy
-    cv2.line(frame, (x, y), (x+40, y+10), red, 2, cv2.LINE_AA)    
+    cv2.line(frame, (x, y), (x+40, y+10), red, 2, cv2.LINE_AA)
